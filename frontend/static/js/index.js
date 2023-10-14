0afb787006fb999fa763ddb7b0dbb8e8c373b5c9
hudsonviana <hudson1206@gmail.com>
2023-10-14 19:42:41 -0300

    Navegação sem refresh da página concluída

---
+++ frontend/static/js/index.js
@@ -0,0 +1,42 @@
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: '/', view: () => console.log('Viewing Dashboard') },
+    { path: '/posts', view: () => console.log('Viewing Posts') },
+    { path: '/settings', view: () => console.log('Viewing Settings') },
+  ];
+
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    };
+  }
+
+  match.route.view();
+};
+
+window.addEventListener('popstate', router);
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('[data-link]')) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
